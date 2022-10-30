077f27bf61f199a5a97870f4a73c7db48b30adee
Leonardo Pinto <pintoleonardo1023@gmail.com>
2022-10-30 14:44:44 -0500

    Se agregó el footer

--- componentes/footer.php
+++ componentes/footer.php
@@ -1,3 +1,23 @@
+
+<footer>
+    <nav class=" nav fixed-button navbar navbar-expand-lg navbar-light bg-light p-5">
+            <div class="col-4 text-center">
+                <!-- Logo del Sena -->
+                <img src="../bootstrap-5.0.2-dist/img/logo-sena-verde.png" alt="Logo" width="50" >
+            </div>
+            <div class="row">
+            <div class="col-6 text-right">
+                    <!-- GITHUB de Pinto-->
+                    <a class="navbar-brand" href=""> &copy;Leonardo Pinto</a>
+            </div>
+            <div class="col-6 text-right">
+                    <!-- GITHUB de Camargo -->
+                    <a class="navbar-brand" href="#">&copy;Geovanny Camargo</a>
+            </div>
+            </div>
+    </nav>
+</footer>
+
 <!-- Codigo JavaScript -->
 <script src="./bootstrap-5.0.2-dist/js/bootstrap.bundle.js"></script>
 <!-- Buscador -->
