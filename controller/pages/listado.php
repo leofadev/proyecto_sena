f629fdfaf693ade776a5a89328c7642f81d1b488
Leonardo Pinto <pintoleonardo1023@gmail.com>
2022-11-03 07:52:53 -0500

    Cambio de camargo

---
+++ controller/pages/listado.php
@@ -0,0 +1,23 @@
+<!-- Pagina de los listados de registro de objetos y personas -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <link rel="stylesheet" href="../bootstrap-5.0.2-dist/css/sb-admin-2.css">
+    <link rel="stylesheet" href="../bootstrap-5.0.2-dist/css/bootstrap.min.css">
+</head>
+<body class="nav">
+<?php
+    //include("../componentes/navbar.php");
+?>
+<h1>Listado de registros</h1>
+
+</body>
+</html>
+<?php
+    include("../componentes/footer.php");
+?>
---
+++ crud-celador/controller/controlador_cerrar_session.php
@@ -0,0 +1,7 @@
+<!-- Este codigo nos ayuda a cerrar la session de la persona que ingreso al sitio web -->
+
+<!-- 
+session_start();
+session_destroy();
+header("location:../index.php");
+?> -->
