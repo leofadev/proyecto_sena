0d3ab8d17e500b68879e158ac944b2159fa94960
Geovanny Camargo <geovannyroman2019@gmail.com>
2022-11-11 11:13:14 -0600

    atualizando titulos de las paginas

--- pages/modificar_personas.php
+++ pages/modificar_personas.php
@@ -26,7 +26,7 @@ $sql=$con->query(" SELECT * FROM personas WHERE id=$id ");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Modificar Registro</title>
+    <title>Actualizar Datos Personales</title>
     <!-- Estilos css -->
     
     <link rel="stylesheet" href="../bootstrap-5.0.2-dist/css/sb-admin-2.css">
